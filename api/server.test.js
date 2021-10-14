554d46bfc32edb3b7639d10280368b8ebd74470c
Christina Yun <poncheeyun@yahoo.com>
2021-10-14 16:17:23 -0700

    Created tests and now going to fill out the router

--- api/server.test.js
+++ api/server.test.js
@@ -2,7 +2,34 @@ const server = require('./server');
 const request = require('supertest');
 const db = require('./../data/dbConfig');
 
+beforeAll(async() =>{
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+
+afterAll(async() => {
+  await db.destroy();
+})
 
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+describe('[POST] /register', () => {
+  let res
+  beforeEach(async() => {
+    res = await request(server).post('/auth/register').send({ username: 'pincess_serenity', password: "fairies" })
+  })
+
+  it('responds with a 201 created', () => {
+    expect(res.status).toBe(201)
+  })
+  it.todo('causes a user to be added to the db')
+  it.todo('responds with newly created user')
+})
+
+describe('[POST] /login', () => {
+  it.todo('responds with a status 200')
+  it.todo('has a message')
+  it.todo('has a token')
 })
